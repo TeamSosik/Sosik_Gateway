da47dcda578a874008058f5810be57d437aa37d7
ComNewbie <iforking3@naver.com>
2023-12-19 13:45:04 +0900

    #feat 게이트웨이 초기 설정

---
+++ sosik-gateway/src/test/java/com/example/SoSikGateway/SoSikGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoSikGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoSikGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
