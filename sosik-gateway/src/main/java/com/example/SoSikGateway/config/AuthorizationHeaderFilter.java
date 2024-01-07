ea622a6323446e7633475879616c55ed8a9d6380
minutaurus <made_power@naver.com>
2024-01-07 20:47:36 +0900

    feat : api 게이트웨이를 통한 인증
    기존 멤버 서비스에서 진행하던 인증작업을 api 게이트웨이를 통해 할 수 있도록 구현하였습니다. Redis를 직접 연결하였고 멤버서비스의 로직을 최대한 활용하였습니다.

---
+++ sosik-gateway/src/main/java/com/example/SoSikGateway/config/AuthorizationHeaderFilter.java
@@ -0,0 +1,65 @@
+package com.example.SoSikGateway.config;
+
+import com.example.SoSikGateway.util.JwtTokenUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+    private final JwtTokenUtils jwtTokenUtils;
+
+    public AuthorizationHeaderFilter( JwtTokenUtils jwtTokenUtils){
+        super(Config.class);
+        this.jwtTokenUtils = jwtTokenUtils;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+
+        return (exchange, chain) -> {
+            if (exchange.getRequest().getURI().getPath().contains("/login") ||
+                exchange.getRequest().getURI().getPath().contains("/sign-up")) {
+                return chain.filter(exchange);
+            }
+
+            ServerHttpRequest request = exchange.getRequest();
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) return onError(exchange,
+                    "No authorization header", HttpStatus.UNAUTHORIZED);
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer ", "");
+            try{
+                jwtTokenUtils.validateToken(jwt);
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        log.error(err);
+        return response.setComplete();
+    }
+    private boolean isJwtValid(String jwt) {
+        String subject = null;
+        try {
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return !Strings.isBlank(subject);
+    }
+    public static class Config {}
+}
